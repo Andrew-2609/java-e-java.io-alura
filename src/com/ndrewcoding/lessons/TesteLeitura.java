b96094431cbee2c6f4b9b12d2b634270edd072c0
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-09 22:26:33 -0300

    Feat: add basic implementation of TesteLeitura

--- src/com/ndrewcoding/lessons/TesteLeitura.java
+++ src/com/ndrewcoding/lessons/TesteLeitura.java
@@ -1,4 +1,19 @@
 package com.ndrewcoding.lessons;
 
+import java.io.*;
+
 public class TesteLeitura {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fileInputStream = new FileInputStream("lorem.txt");
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String linha = bufferedReader.readLine();
+
+        System.out.println(linha);
+
+        bufferedReader.close();
+
+    }
 }
