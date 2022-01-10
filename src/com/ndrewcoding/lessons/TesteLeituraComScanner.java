8fe11fa48938d5c90d2cb9c5313dcb9c973f53e5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 09:09:47 -0300

    Feat: implement basic reading with Scanner

--- src/com/ndrewcoding/lessons/TesteLeituraComScanner.java
+++ src/com/ndrewcoding/lessons/TesteLeituraComScanner.java
@@ -1,4 +1,20 @@
 package com.ndrewcoding.lessons;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TesteLeituraComScanner {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("contas.csv"));
+
+        while (scanner.hasNextLine()) {
+            String linha = scanner.nextLine();
+            System.out.println(linha);
+        }
+
+        scanner.close();
+
+    }
 }
