0da607b5156b67d3fd412119a4944852782cd80b
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 07:19:37 -0300

    Feat: add basic implementation of TesteCopiarArquivo

--- src/com/ndrewcoding/lessons/TesteCopiarArquivo.java
+++ src/com/ndrewcoding/lessons/TesteCopiarArquivo.java
@@ -1,4 +1,28 @@
 package com.ndrewcoding.lessons;
 
+import java.io.*;
+
 public class TesteCopiarArquivo {
+    public static void main(String[] args) throws IOException {
+
+        InputStream fileInputStream = new FileInputStream("lorem.txt");
+        Reader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        FileOutputStream fileOutputStream = new FileOutputStream("lorem_copied.txt");
+        Writer outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+        String linha = bufferedReader.readLine();
+
+        while (linha != null) {
+            bufferedWriter.write(linha);
+            bufferedWriter.newLine();
+            linha = bufferedReader.readLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+
+    }
 }
