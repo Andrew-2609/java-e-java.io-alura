4343afcb26b8a8887868c0551499540cd708dda1
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 07:12:29 -0300

    Feat: add basic implementation of TesteEscrita

--- src/com/ndrewcoding/lessons/TesteEscrita.java
+++ src/com/ndrewcoding/lessons/TesteEscrita.java
@@ -1,4 +1,19 @@
 package com.ndrewcoding.lessons;
 
+import java.io.*;
+
 public class TesteEscrita {
+    public static void main(String[] args) throws IOException {
+
+        FileOutputStream fileOutputStream = new FileOutputStream("generated.txt");
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+        bufferedWriter.write("Primeira linha. Olá!");
+        bufferedWriter.newLine();
+        bufferedWriter.write("Segunda linha.");
+
+        bufferedWriter.close();
+
+    }
 }
