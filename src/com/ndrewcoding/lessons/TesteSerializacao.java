d85a29c91bc4cf7a5ee3775dad535ab3cdabc23a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 21:35:23 -0300

    Feat: implement writing and reating of object in TesteSerializacao.java

--- src/com/ndrewcoding/lessons/TesteSerializacao.java
+++ src/com/ndrewcoding/lessons/TesteSerializacao.java
@@ -1,4 +1,21 @@
 package com.ndrewcoding.lessons;
 
+import java.io.*;
+
 public class TesteSerializacao {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        String nome = "Andrew Monteiro";
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
+        objectOutputStream.writeObject(nome);
+        objectOutputStream.close();
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("objeto.bin"));
+        String value = objectInputStream.readObject().toString();
+        objectInputStream.close();
+
+        System.out.println(value);
+
+    }
 }
